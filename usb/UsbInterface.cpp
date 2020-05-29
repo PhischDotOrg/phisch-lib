37968d94e79b764ef2b7bf0054a1f9627dd1d3c9
Philip Schulz <phs@phisch.org>
2020-05-29 21:06:56 +0200

    Make CDC Types class-private Types and make the Line Coding struct a member.

--- usb/UsbInterface.cpp
+++ usb/UsbInterface.cpp
@@ -36,47 +36,7 @@ UsbVcpInterface::disable(void) const {
  *
  ******************************************************************************/
 void
-UsbVcpInterface::handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) const {
-    /*
-     * Table 19: "Class-Specific Request Codes" from "Universal Serial Bus Class Definitions for Communications Devices", Rev. 1.2 (Errata 1) from Nov 3rd, 2010.
-     */
-    enum UsbCdcRequest_e {
-        e_SetLineCoding         = 0x20,
-        e_GetLineCoding         = 0x21,
-        e_SetControlLineState   = 0x22,
-    };
-
-    /**
-     * @brief USB Line Coding Structure
-     * 
-     * Line Coding Structure according to:
-     * 
-     * Table 17 "Line Coding Structure"
-     * 
-     * in
-     * 
-     * "Universal Serial Bus Communications Class Subclass Specification for PSTN Devices", Rev. 1.2 from Feb 9th, 2007.
-     * 
-     * As per the USB Standard, this struct is 7 Bytes long. However, because we're transmitting
-     * OUT Packets in words (i.e. chunks of four bytes) into application memory, this struct should
-     * be aligned on a 4-Byte boundary. This will effectively increase the RAM footprint of the
-     * structure to 8 Bytes.
-     * 
-     * \c see ::usb::stm32f4::OutEndpointViaSTM32F4::dataReceivedDeviceCallback
-     */
-    struct UsbCdcLineCoding_s {
-        union {
-            uint32_t    m_u32;
-            uint8_t     m_u8[4];
-        } m_dwDTERate;
-        uint8_t         m_bCharFormat;
-        uint8_t         m_bParityType;
-        uint8_t         m_bDataBits;
-    } __attribute__((aligned(4), packed));
-    static_assert(sizeof(struct UsbCdcLineCoding_s) >= 7);
-
-    static struct UsbCdcLineCoding_s usbCdcLineCoding = { };
-
+UsbVcpInterface::handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) {
     UsbCdcRequest_e request = static_cast<UsbCdcRequest_e>(p_setupPacket.m_bRequest);
 
     switch (request) {
@@ -86,7 +46,7 @@ UsbVcpInterface::handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) const 
         assert(p_setupPacket.m_wLength == 7);
         assert(p_setupPacket.m_bmRequestType == 0xA1);
 
-        this->m_defaultCtrlPipe->write(reinterpret_cast<const uint8_t *>(&usbCdcLineCoding), sizeof(struct UsbCdcLineCoding_s));
+        this->m_defaultCtrlPipe->write(reinterpret_cast<const uint8_t *>(&m_usbCdcLineCoding), sizeof(struct UsbCdcLineCoding_s));
         break;
     case e_SetControlLineState:
         assert(p_setupPacket.m_bmRequestType == 0x21);
@@ -101,7 +61,7 @@ UsbVcpInterface::handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) const 
         assert(p_setupPacket.m_wLength == 7);
         assert(p_setupPacket.m_bmRequestType == 0x21);
 
-        this->m_defaultCtrlPipe->setDataStageBuffer(reinterpret_cast<uint32_t *>(&usbCdcLineCoding), sizeof(usbCdcLineCoding));
+        this->m_defaultCtrlPipe->setDataStageBuffer(reinterpret_cast<uint32_t *>(&m_usbCdcLineCoding), sizeof(m_usbCdcLineCoding));
         break;
     default:
         assert(false);
--- usb/include/usb/UsbInterface.hpp
+++ usb/include/usb/UsbInterface.hpp
@@ -22,7 +22,7 @@ public:
 
     virtual void    enable(const UsbControlPipe &p_defaultCtrlPipe) = 0;
     virtual void    disable(void) const = 0;
-    virtual void    handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) const = 0;
+    virtual void    handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) = 0;
 };
 
 /*******************************************************************************
@@ -30,6 +30,47 @@ public:
  ******************************************************************************/
 class UsbVcpInterface : public UsbInterface {
 private:
+    /*
+     * Table 19: "Class-Specific Request Codes" from "Universal Serial Bus Class Definitions for Communications Devices", Rev. 1.2 (Errata 1) from Nov 3rd, 2010.
+     */
+    enum UsbCdcRequest_e {
+        e_SetLineCoding         = 0x20,
+        e_GetLineCoding         = 0x21,
+        e_SetControlLineState   = 0x22,
+    };
+
+    /**
+     * @brief USB Line Coding Structure
+     * 
+     * Line Coding Structure according to:
+     * 
+     * Table 17 "Line Coding Structure"
+     * 
+     * in
+     * 
+     * "Universal Serial Bus Communications Class Subclass Specification for PSTN Devices", Rev. 1.2 from Feb 9th, 2007.
+     * 
+     * As per the USB Standard, this struct is 7 Bytes long. However, because we're transmitting
+     * OUT Packets in words (i.e. chunks of four bytes) into application memory, this struct should
+     * be aligned on a 4-Byte boundary. This will effectively increase the RAM footprint of the
+     * structure to 8 Bytes.
+     * 
+     * \c see ::usb::stm32f4::OutEndpointViaSTM32F4::dataReceivedDeviceCallback
+     */
+    struct UsbCdcLineCoding_s {
+        union {
+            uint32_t    m_u32;
+            uint8_t     m_u8[4];
+        } m_dwDTERate;
+        uint8_t         m_bCharFormat;
+        uint8_t         m_bParityType;
+        uint8_t         m_bDataBits;
+    } __attribute__((aligned(4), packed));
+    static_assert(sizeof(struct UsbCdcLineCoding_s) >= 7);
+    typedef struct UsbCdcLineCoding_s UsbCdcLineCoding_t;
+    
+    UsbCdcLineCoding_t      m_usbCdcLineCoding;
+
     const UsbControlPipe *  m_defaultCtrlPipe;
     UsbBulkOutEndpoint &    m_outEndpoint;
     UsbBulkInEndpoint &     m_inEndpoint;
@@ -46,7 +87,7 @@ public:
     void    enable(const UsbControlPipe &p_defaultCtrlPipe) override;
     void    disable(void) const override;
 
-    void    handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) const override;
+    void    handleCtrlRequest(const UsbSetupPacket_t &p_setupPacket) override;
 };
 
 /*******************************************************************************
