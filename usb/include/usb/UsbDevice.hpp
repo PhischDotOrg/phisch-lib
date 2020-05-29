360e9298e6b132a0a7387a7556f1802da7d30f1c
Philip Schulz <phs@phisch.org>
2020-05-29 21:59:27 +0200

    Refactor Device Descriptor out of class UsbDevice.

--- usb/include/usb/UsbDevice.hpp
+++ usb/include/usb/UsbDevice.hpp
@@ -31,32 +31,17 @@ private:
     uint8_t                                     m_activeConfiguration;
     const ::usb::UsbConfiguration *             m_configurations[m_maxConfigurations];
 
-    const ::usb::UsbDeviceDescriptor_t          m_deviceDescriptor;
+    const ::usb::UsbDeviceDescriptor_t &        m_deviceDescriptor;
     const ::usb::UsbDeviceQualifierDescriptor_t m_deviceQualifierDescriptor;
     const ::usb::UsbStringDescriptors_t &       m_stringDescriptors;
     const ::usb::UsbDeviceStatus_t              m_deviceStatus;
 
 public:
-    typedef const std::initializer_list<::usb::UsbConfiguration *> UsbConfigurationList_t;
-    
-    constexpr UsbDevice(UsbHwDevice &p_hwDevice, const ::usb::UsbStringDescriptors_t &p_stringDescriptors, UsbConfigurationList_t p_configurations)
-      : m_hwDevice(p_hwDevice), m_ctrlPipe(nullptr), m_activeConfiguration(0), m_configurations {},
-        m_deviceDescriptor {
-            sizeof(m_deviceDescriptor),                                     /* m_bLength */
-            ::usb::UsbDescriptorTypeId_t::e_Device,                         /* m_bDescriptorType */
-            { 0x00, 0x02 },                                                 /* m_bLength */
-            ::usb::UsbInterfaceClass_e::e_UsbInterface_Misc_EFh,            /* m_bDeviceClass */
-            0x02,                                                           /* m_bDeviceSubClass */
-            0x01,                                                           /* m_bDeviceProtocol */
-            static_cast<uint8_t>(p_hwDevice.getDeviceSpeed() == ::usb::UsbHwDevice::DeviceSpeed_e::e_UsbFullSpeed ? 64 : 0), /* m_bMaxPacketSize0 */
-            { 0xad, 0xde },                                                 /* m_idVendor */
-            { 0xef, 0xbe },                                                 /* m_idProduct */
-            { 0xfe, 0xca },                                                 /* m_bcdDevice */
-            ::usb::UsbStringDescriptorId_t::e_StrDesc_Manufacturer,         /* e_iManufacturer */
-            ::usb::UsbStringDescriptorId_t::e_StrDesc_Product,              /* e_iProduct */
-            ::usb::UsbStringDescriptorId_t::e_StrDesc_SerialNumber,         /* e_iSerialNumber */
-            m_maxConfigurations                                             /* e_bNumConfigurations */
-        },
+    typedef std::initializer_list<const ::usb::UsbConfiguration *> UsbConfigurationList_t;
+
+    constexpr UsbDevice(UsbHwDevice &p_hwDevice, const UsbDeviceDescriptor_t &p_deviceDescriptor, const ::usb::UsbStringDescriptors_t &p_stringDescriptors, const UsbConfigurationList_t p_configurations)
+      : m_hwDevice(p_hwDevice), m_ctrlPipe(nullptr), m_activeConfiguration(0), m_configurations (),
+        m_deviceDescriptor(p_deviceDescriptor),
         m_deviceQualifierDescriptor(m_deviceDescriptor),
         m_stringDescriptors(p_stringDescriptors),
         m_deviceStatus { 1 }
