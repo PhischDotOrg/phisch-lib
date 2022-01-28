b11082ee22ea5d6aa414f02c7a1da0103220bbff
Philip Schulz <phs@phisch.org>
2022-01-28 22:09:14 +0100

    Encapsulate the debug_setpin() method in a #define

--- phisch/log.h
+++ phisch/log.h
@@ -21,16 +21,20 @@
     #define PHISCH_LOG(...)
 #endif /* defined(NO_LOGGING) */
 
-#if defined(__cplusplus)
-    extern "C" {
-#endif /* defined(__cplusplus) */
+#if defined(HAVE_DEBUG_PINS)
+    #if defined(__cplusplus)
+        extern "C" {
+    #endif /* defined(__cplusplus) */
 
-    void debug_setpin(const unsigned p_pin, const bool p_val);
+        void debug_setpin(const unsigned p_pin, const bool p_val);
 
-#if defined(__cplusplus)
-    } /* extern "C" */
-#endif /* defined(__cplusplus) */
+    #if defined(__cplusplus)
+        } /* extern "C" */
+    #endif /* defined(__cplusplus) */
 
-#define PHISCH_SETPIN(_p, _v) debug_setpin((_p), (_v))
+    #define PHISCH_SETPIN(_p, _v) debug_setpin((_p), (_v))
+#else /* defined(HAVE_DEBUG_PINS) */
+    #define PHISCH_SETPIN(_p, _v)
+#endif /* defined(HAVE_DEBUG_PINS) */
 
 #endif /* _PHISCH_LOG_HPP_5BE05124_F09F_4C37_8F0F_98FB828FBEC0 */
